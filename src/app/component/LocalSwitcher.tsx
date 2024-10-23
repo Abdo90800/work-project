12de2c0394a213a824dc12a632ebebd06abca5ca
Abdocr77 <Engabdo.dev@gmail.com>
2024-10-23 20:37:38 +0300

    Update LocalSwitcher.tsx

--- src/app/component/LocalSwitcher.tsx
+++ src/app/component/LocalSwitcher.tsx
@@ -2,38 +2,33 @@
 
 import { useLocale } from 'next-intl';
 import { useRouter } from 'next/navigation';
-import { ChangeEvent, useTransition, useEffect } from 'react';
-
+import { useTransition, useEffect } from 'react';
+import Link from 'next/link';
+import immg from "../../../public/images/image 439.png";
 export default function LocalSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localeActive = useLocale();
-
-  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextLocale = e.target.value;
-    startTransition(() => {
-      router.replace(`/${nextLocale}`);
-    });
-  };
 
   useEffect(() => {
     const direction = localeActive === 'ar' ? 'rtl' : 'ltr';
     document.documentElement.dir = direction;
   }, [localeActive]);
 
+  const handleLanguageChange = (nextLocale: string) => {
+    startTransition(() => {
+      router.replace(`/${nextLocale}`);
+    });
+  };
+
   return (
-    <label className='border-2 rounded'>
-      <select
-        defaultValue={localeActive}
-        className='py-2 bg-transparent'
-        onChange={onSelectChange}
-        disabled={isPending}
-        aria-label="Select language"
-      >
-        <option value='en'>English</option>
-        <option value='ar'>العربية</option>
-        
-      </select>
-    </label>
+    <div className='flex space-x-4'>
+      <Link href='/en' onClick={() => handleLanguageChange('en')} className='flex items-center'>
+        <img src='/' alt='English' className='w-6 h-6' />
+      </Link>
+      <Link href='/ar' onClick={() => handleLanguageChange('ar')} className='flex items-center'>
+        <img src='/path-to-egypt-flag.png' alt='Arabic' className='w-6 h-6' />
+      </Link>
+    </div>
   );
 }
