ee33077445ce11d432ea73f99e6f6b600120c4d1
Abdocr77 <Engabdo.dev@gmail.com>
2024-10-23 18:23:07 +0300

    add slider and carousel

---
+++ .next/server/_rsc_messages_en_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_en_json";
+exports.ids = ["_rsc_messages_en_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/en.json":
+/*!**************************!*\
+  !*** ./messages/en.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"home":"HOME","about":"ABOUT","contactus":"CONTACT US","aboutus":"ABOUT US","login":"LOGIN","signin":"SIGNIN","blogs":"BLOGS","Carosil1":"Your Space with Our Stylish Solutions","Carosil2":"Transform"}');
+
+/***/ })
+
+};
+;
