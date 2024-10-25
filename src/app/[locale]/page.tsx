812d4880d231e7857c0bc0e10a089fc54ba2e54e
Joo <youssefjoo@gmail.com>
2024-10-26 02:09:36 +0300

    finish our-partners - Section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import { useTranslations } from "next-intl";
 import MainCards from "../component/MainCards";
 import Frames from "../component/Frames";
+import OurPartners from "../component/OurPartners";
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1>{t("title")}</h1>
+      <OurPartners />
       <Frames />
       <MainCards />
     </div>
