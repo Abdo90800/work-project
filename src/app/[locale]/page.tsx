b84d44fa1b77f6bbe4be8ad2e9c2d35bd69f810d
Joo <youssefjoo@gmail.com>
2024-10-26 21:00:04 +0300

    add card slider

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,12 +8,14 @@ import SliderCard from "../component/SliderCard";
 
 
 import OurPartners from "../component/OurPartners";
+import SliderCard from "../component/SliderCard";
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1>{t("title")}</h1>
       <OurPartners />
+      <SliderCard />
       <Frames />
       <MainCards />
       <Caro />
--- src/app/component/SliderCard.jsx
+++ src/app/component/SliderCard.jsx
@@ -2,10 +2,12 @@
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+// import img from "../../../images/card2.png"; // تأكد أن المسار للصورة صحيح
+// import img1 from "../../../images/card1.png"; // تأكد أن المسار للصورة صحيح
+// import img2 from "../../../images/card3.png"; // تأكد أن المسار للصورة صحيح
 import { RiDiscountPercentFill } from "react-icons/ri";
 import { CiHeart } from "react-icons/ci";
-import items from "../../data/item";
-import { useTranslations } from "next-intl";
+// import items from "../../data/item";
 
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -13,8 +15,23 @@ import Box from "@mui/material/Box";
 import Image from "next/image";
 import Link from "next/link";
 function Slider() {
-  const t = useTranslations("");
-
+  const items = [
+    {
+      img: "/images/swipper-1.png",
+      title: "Product Name",
+      rate: "/images/ratingSwipper.png",
+    },
+    {
+      img: "/images/swipper-2.png",
+      title: "Product Name",
+      rate: "/images/ratingSwipper.png",
+    },
+    {
+      img: "/images/swipper-3.png",
+      title: "Product Name",
+      rate: "/images/ratingSwipper.png",
+    },
+  ];
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
@@ -48,14 +65,18 @@ function Slider() {
 
   return (
     <>
-      <Box className="flex items-center justify-between mt-[50px] mx-20 mb-[15px]">
+      <Box className="flex items-center justify-between mt-[50px]  mb-[15px] max-w-screen-xl mx-auto ">
         <Box>
-          <Typography sx={{ fontSize: "20px", fontWeight: "700" }}>
-          {t("Category button")}
-
+          <Typography
+            sx={{ fontSize: "20px", fontWeight: "700", color: "#545F71" }}
+          >
+            Best selling:
           </Typography>
-          <Typography sx={{ fontSize: "14px", fontWeight: "500" }}>
-          {t("Discover")}.
+          <Typography
+            sx={{ fontSize: "14px", fontWeight: "500", color: "#757575" }}
+          >
+            Our products offer the perfect blend of customization, quality, and
+            affordability.
           </Typography>
         </Box>
 
@@ -64,62 +85,64 @@ function Slider() {
             sx={{ background: "#545F71", color: "white" }}
             className="w-[91px] rounded-[10px]"
           >
-        {t("View All")}
+            View All
           </Button>
         </Link>
       </Box>
 
       <div className="flex items-center justify-center">
-        <Carousel
-          responsive={responsive}
-          centerMode={true} // يضمن بقاء الكارد المركزي بين الأسهم
-          className=" w-[1280px] flex justify-center"
-        >
-          {items.map((e) => (
-            <div
-              key={e.id}
-              className="group relative block overflow-hidden lg:w-[416px] lg:h-[590px] sm:w-[200px] sm:h-[450px] md:w-[300px] md:h-[450px]  rounded-[16px] gap-[8px]"
+      <div className="flex items-center justify-center">
+  <Carousel
+    responsive={responsive}
+    centerMode={true} // يضمن بقاء الكارد المركزي بين الأسهم
+    className="w-[1280px] flex justify-center"
+  >
+    {items.map((e) => (
+      <div
+        key={e.id}
+        className="group relative block lg:w-[416px] lg:h-[590px] sm:w-[200px] sm:h-[450px] md:w-[300px] md:h-[450px] rounded-[16px] gap-[8px]"
+      >
+        <button className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
+          <span className="sr-only">Wishlist</span>
+          <CiHeart className="text-[26.18px]" />
+        </button>
+
+        {/* صورة المنتج */}
+        <div className="w-[416px] h-[480px] relative overflow-hidden">
+          <Image
+            src={e.img}
+            alt="photo"
+            className="w-full h-full transition duration-500 group-hover:scale-105 sm:h-72 absolute"
+            fill
+          />
+        </div>
+
+        {/* محتوى الكارد */}
+        <div className="absolute bg-white border border-[#D3D3D3] rounded-b-xl pl-4 pt-1 z-10 w-full h-24 bottom-5">
+          {/* النص والـ Rating */}
+          <p className="text-customheader">{e.title}</p>
+          <div className="mt-1.5 text-lg font-medium text-gray-900">
+            <img src={e.rate} alt="photo" />
+          </div>
+
+          {/* الزرار المعدل */}
+          <form className="flex">
+            <button
+              style={{
+                background: "linear-gradient(to right, #5AE4CB, #3EABCC)",
+              }}
+              type="button"
+              className="w-[116px] h-[40px] rounded-[10px] bg-gray-900 px-4 py-3 text-sm font-medium text-white transition hover:scale-105 ml-auto mr-4"
             >
-              <button className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
-                <span className="sr-only">Wishlist</span>
-                <CiHeart className="text-[26.18px]" />
-              </button>{" "}
-              <button className="absolute left-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
-                <div className="w-[105px] h-[40] bg-white rounded-[20px] p-[8px] gap-[4px] flex items-center">
-                  <RiDiscountPercentFill className="text-[#9B0202] text-[24px] " />
-                  <span className="text-[#9B0202]">15% off</span>
-                </div>
-              </button>
-              <Image
-                src={e.image}
-                alt=""
-                className="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72"
-              />
-              <div className="relative p-6 bg-white border border-gray-100">
-                <p className="text-gray-700">
-                  ${e.price}
-                  <span className="text-gray-400 line-through">$80</span>
-                </p>
+              Order Now
+            </button>
+          </form>
+        </div>
+      </div>
+    ))}
+  </Carousel>
+</div>
 
-                <h3 className="mt-1.5 text-lg font-medium text-gray-900">
-                  {e.title}
-                </h3>
-
-                <form className="flex gap-4 mt-4">
-                  <button
-                    style={{
-                      background: "linear-gradient(to right, #5AE4CB, #3EABCC)",
-                    }}
-                    type="button"
-                    className="w-[116px] h-[40px] rounded-[10px]  bg-gray-900 px-4 py-3 text-sm font-medium text-white transition hover:scale-105"
-                  >
-                {t("Order Now")}
-                  </button>
-                </form>
-              </div>
-            </div>
-          ))}
-        </Carousel>
       </div>
     </>
   );
