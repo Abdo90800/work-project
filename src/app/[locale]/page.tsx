526eee8dcfee2b765ee67257a8e6e52dd51ef66a
Joo <youssefjoo@gmail.com>
2024-10-31 08:33:17 +0300

    add contact us page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,13 @@ import MainCards from "../component/MainCards";
 import Frames from "../component/Frames";
 import OurPartners from "../component/OurPartners";
 import SliderCard from "../component/SliderCard";
+import ContactUs from "../component/ContactUs";
 export default function HomePage() {
   const t = useTranslations("HomePage");
   return (
     <div>
       <h1>{t("title")}</h1>
+      <ContactUs />
       <OurPartners />
       <SliderCard />
       <Frames />
